766ce34c43cb72abeebcb0b38bd1dfeb9f42b42a
Braian <braiandelgado4255@hotmail.com>
2021-06-17 18:22:41 -0300

    mongoose validator config

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,28 +1,51 @@
 const { Schema, model } = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
-const userSchema = new Schema({
-    firstname: {
-        type: String,
+
+const userSchema = new Schema(
+    {
+        firstname: {
+            type: String,
+        },
+        lastname: {
+            type: String,
+        },
+        username: {
+            type: String,
+            unique: true,
+            required: [true, "Username field is required"],
+        },
+        birthdate: {
+            type: Date,
+        },
+        email: {
+            type: String,
+            required: [true, "Email field is required"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password field is required"],
+            min: 6,
+            select: false
+        },
+        role: {
+            type: String,
+            required: true,
+            default: "USER_ROLE",
+            enum: ["USER_ROLE", "ADMIN_ROLE"]
+        },
+        enabled: {
+            type: Boolean,
+            required: true,
+            default: true
+        }
     },
-    lastname: {
-        type: String,
-    },
-    username: {
-        type: String,
-        required: [true, "Username field is required"],
-    },
-    birthdate:{
-        type: Date
-    },
-    email: {
-        type: String,
-        required: [true, "Email field is required"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Password field is required"],
-    }
-},{timestamps: true});
+    { timestamps: true }
+);
+
+userSchema.plugin(uniqueValidator,{message: 'Already exist'})
+userSchema.plugin(mongoosePaginate);
 
 module.exports = model("users", userSchema);
