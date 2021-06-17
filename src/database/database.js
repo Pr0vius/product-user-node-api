0d70abdb2765ca7b0554d4cf5ffc20dbc41b53e5
Braian <braiandelgado4255@hotmail.com>
2021-06-17 18:20:42 -0300

    database error handler

--- src/database/database.js
+++ src/database/database.js
@@ -3,14 +3,18 @@ const { database } = require("../config/index");
 const logger = require('../loaders/logger/winston.logger');
 
 const connectDatabase = async () => {
-    const CONNECTION = await mongoose.connect(database.mongodbURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    });
-
-    logger.info(`DB is CONNECTED ${CONNECTION.connection.host}`);
+    try {
+        const CONNECTION = await mongoose.connect(database.mongodbURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+    
+        logger.info(`DB is CONNECTED ${CONNECTION.connection.host}`);
+    } catch (err) {
+        logger.error(`Error at Database connection\n ${err}`)
+    }
 };
 
 module.exports = connectDatabase;
